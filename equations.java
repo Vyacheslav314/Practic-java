5926284730900eef92fc124f83e85408f0612a41
Vyacheslav <vyacheslav.trubaev@mail.ru>
2023-01-29 18:15:13 +0300

    hw 1

--- equations.java
+++ equations.java
@@ -1,50 +1,49 @@
-import java.sql.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 // *+Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры могут быть заменены знаком вопроса, например 2? + ?5 = 69. 
 // Требуется восстановить выражение до верного равенства. Предложить хотя бы одно решение или сообщить, что его нет.
 
 public class equations {
+
     public static void main(String[] args) {
         String inputStr = inputString();
-        String newStr = lineChanges(inputStr);
-        String[] array = createArray(newStr);
-        System.out.println(java.util.Arrays.toString(array));
-        System.out.println(solutionEquation(array));
+        String[] array = createArray(inputStr);
+        System.out.println(String.join(" ", array));   
     }
 
     static String inputString(){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Введите уровнение пример: q + w = e\nобратите внимание все значения вводятся без пробелов пример: 2?+?5=69\n");
+        System.out.println("Введите уровнение пример: q + w = e\nобратите внимание все значения вводятся через пробел пример: 2? + ?5 = 69");
         String equat = scan.nextLine();
         return equat;
     }
 
-    static String lineChanges(String str){
-        String str1 = "";
-        for (int j = 0; j < str.length(); j++) {
-            if(str.charAt(j) != '?'){
-                str1 += str.charAt(j);
-            }
-        }
-        return str1;
-    }
-
     static String[] createArray(String str){
         String[] array = str.split(" ");
+        int i = 0;
+        int j = 0;
+        Boolean isValid = true;
+        while (isValid) {
+            array = str.split(" ");
+            array[0] = array[0].replace("?", String.valueOf(i));
+            array[2] = array[2].replace("?", String.valueOf(j));
+            if(Integer.parseInt(array[0]) + Integer.parseInt(array[2]) != Integer.parseInt(array[4])){
+                j++;
+                if (j > 9) {
+                    j = 0;
+                    i++;
+                }
+                if(i > 9){
+                    System.out.printf("Уровнение %s не имеет решения", str);
+                    System.exit(1);
+                }
+            }else{
+                System.out.printf("%s найдено верное равенство ", str);
+                isValid = false;
+            }
+        }
         return array;
     }
 
-    static String solutionEquation(String[] arr){
-        String str = "";
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] == ){
-                str += arr[i];
-            }
-
-            
-        }   
-        System.out.println(str);
-        return "1";
-    }
 }
