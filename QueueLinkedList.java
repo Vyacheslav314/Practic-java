b89685cb2002a7a89acb10756a8b37a2c4922840
Vyacheslav <vyacheslav.trubaev@mail.ru>
2023-02-07 20:39:36 +0300

    complited

--- QueueLinkedList.java
+++ QueueLinkedList.java
@@ -1,37 +1,42 @@
 // Реализуйте очередь с помощью LinkedList со следующими методами:enqueue() - помещает элемент в конец очереди,
 // dequeue() - возвращает первый элемент из очереди и удаляет его, first() - возвращает первый элемент из очереди, не удаляя.
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.List.*;
 
 
 public class QueueLinkedList {
+
     public static void main(String[] args) {
         Queue<Integer> queue = new LinkedList<Integer>();
         enqueue(queue, 17);
         enqueue(queue, 12);
         enqueue(queue, 22);
-        enqueue(queue, 7);
+        enqueue(queue, 2);
+        enqueue(queue, 33);
         System.out.println(queue);
-        int elem = dequeue(queue);
-        System.out.println(elem);
+        System.out.println(dequeue(queue));
         System.out.println(queue);
-        int elem2 = first(queue);
-        System.out.println(elem2);
-        System.out.println(queue);
+        System.out.println(first(queue));
     }
     
     public static void enqueue(Queue<Integer> queue, int item) {
         queue.add(item);
     }
 
-    public static int dequeue(Queue<Integer> queue){
-        int elem = ((LinkedList<Integer>) queue).getFirst();
-        ((LinkedList<Integer>) queue).pollFirst();
-        return elem;
+    public static Integer dequeue(Queue<Integer> queue){
+        for (Iterator<Integer> i = queue.iterator(); i.hasNext();) {
+            Integer number = i.next();
+            if(number != null){
+                queue.remove();
+                return number;
+            }
+        }
+        return null;
     }
 
     public static int first(Queue<Integer> queue) {
-        return ((LinkedList<Integer>) queue).getFirst();
+        return queue.peek();
     }
 }
--- calculate.java
+++ calculate.java
@@ -4,89 +4,109 @@ import java.util.logging.*;
 
 // Реализовать простой калькулятор
 // добавить логирование
+// В калькулятор добавьте возможность отменить последнюю операцию.
 
-public class calculate {
-    
-    private static Logger logger = Logger.getLogger(calculate.class.getName());
+public class Calculate {
+
+    private static Logger logger = Logger.getLogger(sorted.class.getName());
+    static Scanner scanner = new Scanner(System.in);
 
     public static void someMethod() throws SecurityException, IOException{
-        FileHandler fh = new FileHandler("calc.log", true);
+        FileHandler fh = new FileHandler("calc.log",true);
         logger.addHandler(fh);
         SimpleFormatter sFormat = new SimpleFormatter ();
         fh.setFormatter(sFormat); 
     }
 
-
     public static void main(String[] args) throws SecurityException, IOException {
-       
         someMethod();
-        logger.info("Start program ");
-        float numA = 0;
-        float numB = 0;
+        logger.info("Старт программы! ");
+        double operand1 = nextInt();
+        double operand2 = nextInt();
+        char operation = getOperation();
         double result = 0;
-        while (true) {
-            int command = inputNumber("Выберите номер команды\n1.Сложение\n2.Вычитание\n3.Умножение\n4.Деление\n5.Завершить работу\n");
-            switch (command) {
-                case 1:
-                    logger.info("Выбранна операция сложение ");
-                    numA = inputNumber("Введите число a ");
-                    logger.info("Введенно число " + numA);
-                    numB = inputNumber("Введите число b ");
-                    logger.info("Введенно число " + numB);
-                    result = operationResalt(numA, numB, command);
-                    logger.info("Результат сложения " + result);
+        boolean bool = true;
+        while (bool) {
+            switch (operation) {
+                case '+':
+                    result = operand1 + operand2;
+                    logger.info("Результат " + operand1 + " " + operation + " " + operand2 + " = " + result);
                     break;
-                case 2:
-                    logger.info("Выбранна операция вычитание ");
-                    numA = inputNumber("Введите число a ");
-                    logger.info("Введенно число " + numA);
-                    numB = inputNumber("Введите число b ");
-                    logger.info("Введенно число " + numB);
-                    result = operationResalt(numA, numB, command);
-                    logger.info("Результат сложения " + result);
+                case '-':
+                    result = operand1 - operand2;
+                    logger.info("Результат " + operand1 + " " + operation + " " + operand2 + " = " + result);
                     break;
-                case 3:
-                    logger.info("Выбранна операция умножение ");
-                    numA = inputNumber("Введите число a ");
-                    logger.info("Введенно число " + numA);
-                    numB = inputNumber("Введите число b ");
-                    logger.info("Введенно число " + numB);
-                    result = operationResalt(numA, numB, command);
-                    logger.info("Результат сложения " + result);
+                case '*':
+                    result = operand1 * operand2;
+                    logger.info("Результат " + operand1 + " " + operation + " " + operand2 + " = " + result);
                     break;
-                case 4:
-                    logger.info("Выбранна операция деление ");
-                    numA = inputNumber("Введите число a ");
-                    logger.info("Введенно число " + numA);
-                    numB = inputNumber("Введите чилсо b ");
-                    logger.info("Введенно число " + numB);
-                    result = operationResalt(numA, numB, command);
-                    logger.info("Результат сложения " + result);
+                case '/':
+                    result = operand1 / operand2;
+                    logger.info("Результат " + operand1 + " " + operation + " " + operand2 + " = " + result);
                     break;
-                case 5:
-                    logger.info("Работа приложения завершена!!!");
-                    return;
                 default:
-                    logger.warning("Введены некорректные данные ");
-                    System.out.println("Вы ввели не верную команду попробуйте ещё раз");
+                    logger.warning("Полученна некорректная операция ");
+                    System.out.println("Операция не распознана. Повторите ввод.");
+                    operation = getOperation();
+                    logger.info("Полученна новая операция " + operation);
                     break;
             }
+            logger.info("Выбор команды для дальнейшей работы ");
+            System.out.println("\nВведите команду\n1.Продолжить работу с новыми данными\n2.Удалить последнюю операцию\n" +
+            "3.Продолжить работу с полученным результатом\n4.Завершить работу программы ");
+            int command = nextInt();
+            if (command == 1) {
+                logger.info("Получение новых данных ");
+                operand1 = nextInt();
+                operand2 = nextInt();
+                operation = getOperation();
+            } else if (command == 2) {
+                logger.info("Удаление пред идущей операции " + operation);
+                operation = getOperation();
+            } else if (command == 3) {
+                logger.info("Продолжаем работать с полученным результатом ");
+                operand1 = result;
+                operand2 = nextInt();
+                operation = getOperation();
+            }else if(command == 4){
+                bool = false;
+                logger.info("Завершение работы программы ");
+            }else{
+                logger.warning("Полученна некорректная команда ");
+                command = nextInt();
+                logger.info("Полученна новая команда " + command);
+            }
+            
         }
     }
 
-    static Integer inputNumber(String str) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print(str);
-        int num = scan.nextInt();
-        return num;
+    public static int nextInt() {
+        System.out.println("Введите число:");
+        int operand;
+        if (scanner.hasNextInt()) {
+            operand = scanner.nextInt();
+            logger.info("Введено число " + operand);
+        } else {
+            logger.warning("Допущена ошибка при вводе данных ");
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            scanner.next();
+            operand = nextInt();
+        }
+        return operand;
     }
 
-    static Float operationResalt(float a, float b, int com){
-        float result = 0;
-        if(com == 1) result = a + b;
-        else if(com == 2) result = a - b;
-        else if(com == 3) result = a * b;
-        else result = a / b;
-        return result;
+    public static char getOperation() {
+        System.out.println("Введите операцию:");
+        char operation;
+        if (scanner.hasNext()) {
+            operation = scanner.next().charAt(0);
+            logger.info("Получен тип операции " + operation);
+        } else {
+            logger.info("Допущена ошибка при вводе данных ");
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
     }
 }
